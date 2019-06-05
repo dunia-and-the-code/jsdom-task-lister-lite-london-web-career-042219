5004cb187215dea7ec68006078965d415b5361c6
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-06-05 16:32:34 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const createTaskForm = document.querySelector('#create-task-form')
+const submitBtn = document.querySelector('#submit')
+const inputBox = document.querySelector('#inputBox')
+const list = document.querySelector('#list')
+
+function submitTask(e) {
+  e.preventDefault()
+  const task = document.createElement('li')
+  task.innerHTML = inputBox.value
+
+  const deleteBtn = document.createElement('button')
+  deleteBtn.innerText = 'x'
+
+  task.append(deleteBtn)
+  list.append(task)
+  createTaskForm.reset()
+
+  deleteBtn.addEventListener('click', () => task.remove())
+}
+
+createTaskForm.addEventListener('submit', submitTask)
+
 });
